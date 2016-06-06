82912b33af904e09813bbb88756375393721dd05
Mafalda Falcão <mafaldafalcaotvf@gmail.com>
2016-06-06 04:27:08 +0100

    forgot this

--- testfile.java
+++ testfile.java
@@ -1,15 +1,23 @@
-public class HelloWorld {
+import java.util.*;
+public class outputFile {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        ArrayList<Object> a = new ArrayList<>();
+        a.add(1);
+        a.add(2);
+        a.add(3);
+        ArrayList<Object> b = new ArrayList<>();
+        b.add(1);
+        b.add(2);
 /*@SETDSL
- Input b;
- Input a;
- Input c;
- Output x;
- x = a';
- Output e = a.b+c int d;
- b = a[5,9];
- c.dump();
+Input a;
+Input b;
+Output c = a int b;
  */
+        for(Object i : c)
+        {
+            System.out.println(i);
+        }
+
+
     }
 }

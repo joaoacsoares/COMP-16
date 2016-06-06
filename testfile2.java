860e460a0762fe6254b3749b1ec9db3a48ba3f50
JoaoSoares <joaosoaresacs@gmail.com>
2016-06-06 06:12:12 +0100

    tf2

---
+++ testfile2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class outputFile {
+    public static void main(String[] args) {
+        ArrayList<Object> a = new ArrayList<>();
+        a.add(1);
+        a.add(2);
+        a.add(3);
+
+/*@SETDSL
+Input a;
+a.dump();
+*/
+      a.add(4);
+        System.out.println();
+        /*@SETDSL
+    Input a;
+    a.dump();
+*/
+    }
+}

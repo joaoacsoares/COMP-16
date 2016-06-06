e3cdb391737fe72be5440ca159a52f3266a7bb05
AndreMachado94 <andremachado94@gmail.com>
2016-06-06 06:11:06 +0100

    testFiles

---
+++ testUndVar.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class outputFile {
+    public static void main(String[] args) {
+        ArrayList<Object> a = new ArrayList<>();
+        a.add(1);
+        a.add(2);
+        ArrayList<Object> b = new ArrayList<>();
+        b.add(3);
+        b.add(4);
+/*@SETDSL
+Input a;
+Input b;
+c = a . b;
+
+*/
+    }
+}
---
+++ testVarTypeMis.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class outputFile {
+    public static void main(String[] args) {
+        ArrayList<Object> a = new ArrayList<>();
+        a.add(1);
+        a.add(2);
+        ArrayList<Object> b = new ArrayList<>();
+        b.add(3);
+        b.add(4);
+/*@SETDSL
+Input a = Set(10);
+Input b = Stack(10);
+Output c;
+c = a . b;
+
+*/
+    }
+}
